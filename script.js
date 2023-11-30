269866b05a5eef1e1e18928d17c652bd192a5173
Nirmal Patra <nirmalpatra408@gmail.com>
2023-11-30 16:01:51 +0530

    Added localStorage to store previous data

--- script.js
+++ script.js
@@ -2,6 +2,17 @@ const notesContainer = document.querySelector(".notes-container");
 const createBtn = document.querySelector(".btn");
 let notes = document.querySelectorAll(".input-box");
 
+function updateStorage() {
+  //to store the notes in LocalStorage
+  localStorage.setItem("notes", notesContainer.innerHTML);
+}
+
+//some how we close the browser and open once again then this function will check whether there is any data present inside the localStorage then it will display it as a note
+function showNotes() {
+  notesContainer.innerHTML = localStorage.getItem("notes");
+}
+showNotes();
+
 createBtn.addEventListener("click", () => {
   let inputBox = document.createElement("p");
   let img = document.createElement("img");
@@ -14,5 +25,15 @@ createBtn.addEventListener("click", () => {
 notesContainer.addEventListener("click", (e) => {
   if (e.target.tagName === "IMG") {
     e.target.parentElement.remove();
+    updateStorage();
+  }
+  //this will update the notes in the localStorage while writing in the notes box
+  else if (e.target.tagName === "P") {
+    notes = document.querySelectorAll(".input-box");
+    notes.forEach((nt) => {
+      nt.onkeyup = () => {
+        updateStorage();
+      };
+    });
   }
 });
